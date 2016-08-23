74b58cfc7a218509e886f7dd9faf73366ded358e
ANTBOT001 <584860790@qq.com>
2016-08-23 16:57:51 +0800

    Create ABTTEST.ino
    ABT蓝牙主板最简程序。
    如果去掉“//abtKits.ABTSimple();return;”这一行代码之前的注释符，主板就可以作为一个USB转TTL串口工具来使用，把跳线帽按在JP3和JP4上，配合串口助手之类的软件，可以实现对蓝牙模块的AT指令设置。

---
+++ ABTTEST.ino
@@ -0,0 +1,25 @@
+#include <ABTKITS.h>
+#include "SoftwareSerial.h"
+
+
+ABTKITS abtKits;
+int cnt=0;
+void setup() {
+  // put your setup code here, to run once:
+abtKits.ABTINIT();//初始化
+delay(200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //abtKits.ABTSimple();return;
+  int i=0;
+  i = abtKits.ABTGetBleCmd();
+  delay(30);
+
+  if(i>5)//接收字节数大于6时执行处理命令函数
+  {
+    abtKits.ABTHandleBleCmd();delay(30);
+   }
+
+}
